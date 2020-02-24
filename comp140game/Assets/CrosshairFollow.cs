2371cc67362cd42919b7251147ca1763cfa02c35
EugeneFalmouth <jsyrickis@gmail.com>
2020-02-24 14:47:51 +0000

    Initial commit, finding assets and setting up the a basic scene with crosshair following the mouse

---
+++ comp140game/Assets/CrosshairFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Image crosshair;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        crosshair.rectTransform.position = Input.mousePosition;
+    }
+}
