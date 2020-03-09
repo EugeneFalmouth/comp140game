64df33c166489b59021b1c4ae3f118d7083232a3
EugeneFalmouth <js231622@falmouth.ac.uk>
2020-03-09 12:19:35 +0000

    Added a score mechanic and enemies dying gives you score.

--- comp140game/Assets/EnemyScript.cs
+++ comp140game/Assets/EnemyScript.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    [SerializeField]
+    private int scoreReward = 5;
+
+    private UIBehaviour ui;
+    
+    private void Start()
+    {
+        ui = FindObjectOfType<UIBehaviour>();
+    }
+    
     public void Die()
     {
         //Work in progress. Add animation or motion of dying
+        ui.UpdateScore(scoreReward);
         Destroy(gameObject);
     }
 }
---
+++ comp140game/Assets/UIBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText;
+
+    private int score = 0;
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        scoreText.text = score.ToString();
+    }
+}
